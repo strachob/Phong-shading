22dea93d25fc12efd6ec6fd7a91c5b13a1aa9ae4
Bartosz Strachowski <bstrachowski@gmail.com>
2019-03-30 12:28:40 +0100

    Add WPF project with canvas

---
+++ Phong-Shading/Phong-Shading/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Phong_Shading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyStrokeDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+            {
+                
+            }
+            else if (e.Key == Key.A)
+            {
+                
+            }
+            else if (e.Key == Key.S)
+            {
+               
+            }
+            else if (e.Key == Key.D)
+            {
+               
+            }
+            else if (e.Key == Key.Q)
+            {
+                
+            }
+            else if (e.Key == Key.E)
+            {
+                
+            }
+        }
+    }
+}
