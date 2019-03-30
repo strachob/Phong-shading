a8af360e709610421844b6a89291976ebe3c5139
Bartosz Strachowski <bstrachowski@gmail.com>
2019-03-30 13:19:50 +0100

    Add Surface class

---
+++ Phong-Shading/Phong-Shading/Models/Surface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phong_Shading.Models
+{
+    public class Surface
+    {
+        public double KDirectional { get; set; }
+        public double KScattered { get; set; }
+        public double Smoothness { get; set; }
+    }
+}
